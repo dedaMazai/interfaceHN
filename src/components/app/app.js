da6c795561487f4e5afddafdf7ae0307b23e70f2
Andrey <andryukha.chipizybov@mail.ru>
2021-01-27 10:30:18 +0300

    правки

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import MainPage from '../mainPage';
 import Article from '../article';
-import Header from '../header';
-import { Link } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
 
 import './app.css';
@@ -12,7 +10,6 @@ export default class App extends Component {
     render() {
         return (
             <>
-                <Link to = "/"><Header/></Link>
                 <Switch>
                     <Route path = '/' exact component={MainPage}/>
                     <Route path = '/card' exact component={Article}/>
